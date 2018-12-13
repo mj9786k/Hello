1fbdeb981bf4fc25af01a6a2a37bba95ff3d6d97
Muhamed Jobarteh <44779592+mj9786k@users.noreply.github.com>
2018-12-13 14:33:22 +0000

    Update 03HelloNameWelcome.py
    Added a variable called campus

--- 03HelloNameWelcome.py
+++ 03HelloNameWelcome.py
@@ -1,6 +1,7 @@
 name = input("What is your name? ")
-course = "COMP1753"
-print("Hello " + name + " - welcome to " + course)
+course = "COMP1715"
+campus = "Greenwich"
+print("Hello " + name + " - welcome to " + campus + course)
 
 print()
 input("Press return to continue ...")
